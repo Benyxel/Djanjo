5366ca6d729ecad1944fa7c248e3a1588d20e287
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-02-19 23:50:16 +0000

    new

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,20 +1,19 @@
 from django.shortcuts import render , redirect
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.messages import error, success
-# Create your views here.
+from django.contrib import messages
 
 def home(request):
     if request.method=='POST':
         username = request.POST['username']
         password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
+        user = authenticate(request, username = username, password = password)
         if user is not None:
             login(request, user)
-            success(request, 'You have successfully logged in')
+            messages.success(request, 'You have successfully logged in')
             return redirect('home')
         else:
-            error(request, 'Invalid username or password')
-            return render(request, 'home')
+            messages.error(request, 'Invalid username or password')
+            return render(request, 'home.html')
     else:   
         return render(request, 'home.html')
 
