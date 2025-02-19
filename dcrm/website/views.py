2ac1b41ef22776054503413406641a7ba73fbceb
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-02-19 19:57:06 +0000

    new

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
-
+from django.shortcuts import render , redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.messages import error, success
 # Create your views here.
+
+def home(request):
+    if request.method=='POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            success(request, 'You have successfully logged in')
+            return redirect('home')
+        else:
+            error(request, 'Invalid username or password')
+            return render(request, 'home')
+    else:   
+        return render(request, 'home.html')
+
+def login_user(request):
+    pass
+
+def logout_user(request):
+    pass
+
