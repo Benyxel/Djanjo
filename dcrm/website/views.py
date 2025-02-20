6e1fc6824f51f719e7864a76eeb28f61b6855f02
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-02-20 01:10:33 +0000

    new

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render , redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
+
+
 
 def home(request):
     if request.method=='POST':
@@ -40,4 +43,18 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
-    return render(request, 'register.html')
+    if request.method=='POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username = username, password = password)
+            login(request, user)
+            messages.success(request, "You have successfully Registered")
+            return redirect('home')
+        else:
+            form = SignUpForm
+            return render(request, 'register.html', {'form':form})
+        
+    
