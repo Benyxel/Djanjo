56f2416c7446595ce1ab3748095660894ca6801e
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-02-20 00:35:13 +0000

    new

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -38,3 +38,6 @@ def logout_user(request):
     logout(request)
     messages.success(request, 'You have successfully logged out')
     return redirect('home')
+
+def register_user(request):
+    return render(request, 'register.html')
