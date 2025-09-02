f2959e9c7c929651bc64d17c8b67c46760558760
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2025-09-02 14:02:11 +0000

    new

--- BUY_SELL_PROJECT/Buy_sell/website/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from .import views
-
-urlpatterns =[
-    path('', views.home, name='home'),
-    path('register', views.register, name='register'),
-    path('login.html', views.login, name='login')
-]
